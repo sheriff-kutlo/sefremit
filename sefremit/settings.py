ae936d03cfc078deabfd14584fefad07f00ef921
Kutlo Mangwa <willmangwa@gmail.com>
2024-08-22 15:53:03 +0200

    updated log file

--- sefremit/settings.py
+++ sefremit/settings.py
@@ -180,8 +180,8 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',  # Set the desired logging level
             'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs', 'sefremit.log'),
-            # 'filename': '/var/log/gunicorn/sefalana_trading.log',
+            # 'filename': os.path.join(BASE_DIR, 'logs', 'sefremit.log'),
+            'filename': '/var/log/gunicorn/sefremit.log',
             'formatter': 'simple',
         },
         'console': {
